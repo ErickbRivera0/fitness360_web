fd333d975b1d06dac056c0788d48c57285669d13
ErickbRivera0 <erickbreydi3007@gmail.com>
2025-06-19 19:10:32 -0600

    V 2.0

--- fitness360_web/src/includes/footer.php
+++ fitness360_web/src/includes/footer.php
@@ -1,6 +1,6 @@
   <footer>
     <div class="container">
-      <p>&copy; 2025 Fitness360. Todos los derechos reservados. Gregory se la come</p>
+      <p>&copy; 2025 Fitness360. Todos los derechos reservados. </p>
     </div>
   </footer>
 </body>

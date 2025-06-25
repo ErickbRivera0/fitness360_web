e52d3626bad9e658151d8b92aa7fdb49d6075d91
mayteh539 <mayteherrera539@gmail.com>
2025-06-24 20:29:21 -0600

    Gregory busca que hacer

--- fitness360_web/src/includes/footer.php
+++ fitness360_web/src/includes/footer.php
@@ -1,6 +1,6 @@
   <footer>
     <div class="container">
-      <p>&copy; 2025 Fitness360. Todos los derechos reservados. </p>
+      <p>&copy; 2025 Fitness360. Todos los derechos reservados Gregory no hace nada. </p>
     </div>
   </footer>
 </body>
